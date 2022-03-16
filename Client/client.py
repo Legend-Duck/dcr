fa093ecc15f8ea9013e65e78a824caa0e1344f1a
None <legendduck666@gmail.com>
2022-03-16 13:05:34 +0800

    modifined client.py & init.py

--- Client/client.py
+++ Client/client.py
@@ -7,8 +7,8 @@ def handler(func):
     return wrapper
 
 class Main(tk.Frame):
-    def __init__(self, root):
-        self.root = root
+    def __init__(self):
+        self.root = tk.Tk()
         super().__init__(self.root)
 
         self.root.title('Duck Chat Room')
@@ -25,6 +25,8 @@ class Main(tk.Frame):
         self.command.grid(row=0, column=1)
 
         self.root.wm_protocol('WM_DELETE_WINDOW', self.client.on_quit)
+
+        self.root.mainloop()
 
         def quit(self):
             self.root.destroy()
@@ -49,11 +51,22 @@ class Command(tk.Frame):
 
         self.client = None
 
-        self.label = tk.Label(self, text='Type /help for commands available.', justify=tk.LEFT, anchor=tk.NW, width=50, height=30, border=5, relief='sunken')
+        self.label = tk.Label(self,
+                              text='Type /help for commands available.',
+                              justify=tk.LEFT,
+                              anchor=tk.NW,
+                              width=50,
+                              height=30,
+                              border=5,
+                              relief='sunken')
         self.label.grid(row=0, column=0)
 
         self.txt = tk.StringVar()
-        self.entry = tk.Entry(self, textvariable=self.txt, width=50, border=5, relief='sunken')
+        self.entry = tk.Entry(self,
+                              textvariable=self.txt,
+                              width=50,
+                              border=5,
+                              relief='sunken')
         self.entry.grid(row=1, column=0)
         self.entry.bind('<Return>', lambda _: self.client.commander())
         self.entry.focus()
@@ -167,11 +180,3 @@ class Client:
             msg = f'[Error] unknown command: {msg}'
         if msg:
             self.updater(msg, self.command)
-
-def main():
-    root = tk.Tk()
-    app = Main(root)
-    root.mainloop()
-
-if __name__ == '__main__':
-    main()
---
+++ Client/init.py
@@ -0,0 +1,7 @@
+import client
+
+def main():
+    app = client.Main()
+
+if __name__ == '__main__':
+    main()
