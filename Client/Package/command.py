ecb7b15131de086c28c5fa30df62bb856a4815a8
None <legendduck666@gmail.com>
2022-03-20 17:16:50 +0800

    client updated command.py

--- Client/Package/command.py
+++ Client/Package/command.py
@@ -1,43 +1,63 @@
-from re import match
+from . import sys_msg
+from re import match, compile
 
 class Command:
     def __init__(self, gui, client):
         self.gui = gui
         self.client = client
-        self.pattern = r'^/([^ ]+)? *((?<= ).+$)?'
+        self.command_pattern = compile(r'^/([^ ]+)? *((?<= ).+$)?')
+        self.dash_pattern = compile(r'-[^-]+')
 
     def update(self, msg):
-        getattr(self.gui, 'update_')(msg, self.gui.command)
+        self.gui.update_(msg, self.gui.command)
 
-    def parsing(self, msg):
-        msg = match(self.pattern, msg)
-        if msg:
-            if msg[1]:
+    def system(self, option, arg=(), num=0):
+        return sys_msg.return_msg(option, arg, num)
+
+    def command_parse(self, cmd):
+        cmd = self.command_pattern.match(cmd)
+        if cmd:
+            if cmd[1]:
                 try:
-                    call = getattr(self, msg[1])
-                    call(msg[2].replace(' ', '')) if msg[2] else call()
+                    call = getattr(self, cmd[1])
+                    call(cmd[2].replace(' ', '')) if cmd[2] else call()
                 except AttributeError:
-                    msg = f'unknown command: {msg[1]}'
+                    cmd = self.system(option='uk_cmd', arg=cmd[1])
                 except TypeError:
-                    msg = f'unknown argument(s): {msg[2] or ""}'
+                    cmd = self.system(option='uk_arg', arg=cmd[2] or '')
                 else:
                     return
             else:
-                msg = 'did you forget to put command?'
+                cmd = self.system(option='no_cmd')
         else:
-            msg = f'did you forget to put \'/\'?'
-        self.update(f'[Error] {msg}')
+            cmd = self.system(option='no_/')
+        self.update(cmd)
+
+    def dash_parse(self, arg):
+        arg = self.dash_pattern.finditer(arg)
+
+    def help(self):
+        pass
 
     def connect(self, addr):
-        try:
-            host, port = addr.split(',')
-        except ValueError:
-            self.update(f'[Error] unknown arguments: {addr}')
+        if self.client.connected:
+            msg = self.system(option='already_con')
         else:
-            self.client.connect(host, port)
+            try:
+                host, port = addr.split(',')
+                port = int(port)
+            except ValueError:
+                msg = self.system(option='uk_arg', arg=addr)
+            else:
+                self.client.connect(host, port)
+                return
+        self.update(msg)
 
     def close(self):
-        self.client.close()
+        if self.client.connected:
+            self.client.close(disconnect=True)
+        else:
+            self.update(self.system(option='not_con'))
 
     def rename(self):
         pass
