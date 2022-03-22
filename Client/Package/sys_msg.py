4745a0671a2d5c2c254fcd973564b8e310511cc5
None <legendduck666@gmail.com>
2022-03-23 01:01:29 +0800

    added help message

--- Client/Package/sys_msg.py
+++ Client/Package/sys_msg.py
@@ -7,14 +7,28 @@ error_msg = {
     'uk_cmd': 'unknown command: %s',
     'uk_arg': 'unknown argument(s): %s',
     'uk_addr': 'unknown address: %s, %s',
-    'uk': '%s',
+    'uk': '%s'
 }
 
 normal_msg = {
     '': '',
 }
 
+help_msg = {
+    'active': 'show the number of active user',
+    'close': 'disconnect from server',
+    'connect {host},{port}': 'connect to server',
+    'rename': 'set a new username',
+    'user {name}': 'show description of that user',
+    'server': 'show info of the server',
+}
+
 def return_msg(option, arg=(), num=0):
     preset = ('[Error] ', '')[num]
     value = (error_msg, normal_msg)[num][option] % arg
     return f'{preset}{value}'
+
+def return_help():
+    func = lambda item: f'{item[0]} - {item[1]}'
+    map_msg = map(func, help_msg.items())
+    return '\n'.join(map_msg)
